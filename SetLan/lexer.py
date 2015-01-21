d3890172b6bc62fe5d2a43c2648605946acef4e3
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2015-01-20 22:47:57 -0430

    Added all tokens. Starting editing formal definitions (the t_ part of every token).

--- SetLan/lexer.py
+++ SetLan/lexer.py
@@ -1,4 +1,11 @@
-tokens = ( 'NAME','NUMBER', 'PLUS','MINUS','TIMES','DIVIDE','EQUALS', 'LPAREN','RPAREN', ) 
+tokens = ('ID','PROGRAM','LCURLY','RCURLY','USING','INT','COLON','SCAN',
+          'LPAREN','RPAREN','PRINTLN','PRINT','IN','IF','ELSE','DQUOTE',
+          'LESSTHAN','GREATERTHAN','LESSTHANE','GREATERTHANE','EQUALS',
+          'COMMA','ASSIGN','PLUS','HASHATAG','NEQUALS','CONTAINS','TRUE',
+          'FALSE','BOOL','MAX','MIN','LEN','SUM','RES','MUL','DIV','MOD',
+          'UNION','DIFFERENCE','INTERSECT','MINUS','TIMES','DIVIDE','MODULUS',
+          'MAXINT','MININT','NUMBER','FOR','RETURN','DEF','ARROW','SEMICOLON',
+          'REPEAT','WHILE','SET','') 
 # Tokens 
 t_PLUS = r'\+' 
 t_MINUS = r'-' 
@@ -7,7 +14,10 @@ t_DIVIDE = r'/'
 t_EQUALS = r'=' 
 t_LPAREN = r'\(' 
 t_RPAREN = r'\)' 
-t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*' 
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+
+t_ID = r'[a-zA-Z_][a-zA-Z0-9_]*' 
 
 def t_NUMBER(t): 
     r'\d+' 
