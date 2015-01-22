a2f238cc15698711e22e9a0a97aefcd827f0c8db
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2015-01-21 22:15:33 -0430

    Completed both sample test cases (the good one and the error-finding one).

--- SetLan/TestSuite.py
+++ SetLan/TestSuite.py
@@ -4,24 +4,24 @@ from setlan import main
 class TestSuite(unittest.TestCase):
     def testSimple(self):
         self.assertEqual(main('Tests/test1.txt'), open('Tests/answer1.txt','r').read())
-        
+
     def testString(self):
         self.assertEqual(main('Tests/test2.txt'), open('Tests/answer2.txt','r').read())
-    
+
     def testString2(self):
         self.assertEqual(main('Tests/test3.txt'), open('Tests/answer3.txt','r').read())
-        
+
     def testSimpleError(self):
         self.assertEqual(main('Tests/test4.txt'), open('Tests/answer4.txt','r').read())
-        
+
     def testCommentAndError(self):
         self.assertEqual(main('Tests/test5.txt'), open('Tests/answer5.txt','r').read())
-        
+
     def testSample1(self):
         self.assertEqual(main('Tests/test6.txt'), open('Tests/answer6.txt','r').read())
-    
+
     def testSample2(self):
-        self.assertEqual(main('Tests/test6.txt'), open('Tests/answer6.txt','r').read())
-    
+        self.assertEqual(main('Tests/test10.txt'), open('Tests/answer10.txt','r').read())
+
 if __name__ == '__main__':
     unittest.main()
